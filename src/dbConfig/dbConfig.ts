4628ff526e796985d3b87236113989106da68776
Ayush <ayush.srivastava@comprotechnologies.com>
2024-03-30 21:35:12 +0530

    First commit

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,25 @@
+import  mongoose  from "mongoose";
+
+export async function connect(){
+    try {
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection=mongoose.connection;
+
+        connection.on('connected',() => {
+            console.log("Connected successfully");
+            
+        });
+
+        connection.on('error',(error) => {
+            console.log('Mongodb connection error');
+            //TODO Study exit
+            process.exit();
+            
+        });
+    } catch (error) {
+        console.log("Something went wrong");
+        console.log(error);
+        
+        
+    }
+}
